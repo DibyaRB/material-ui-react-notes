200567b0c035cb2a976a90a005ef205851d7a589
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-17 21:41:31 +0530

    Testing components and Icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@mui/material/Typography'
+import {Button,Container} from '@mui/material';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+   
+
+      <Container>
+
+     
+        <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+          Create a New Note
+        </Typography>
+
+        <Button
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon/>}
+ 
+        >
+          Submit
+        </Button>
+   
+        </Container>
+  
   )
 }
