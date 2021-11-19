ed1e2299874d8540b1c07321e043b801340d9fcd
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-19 23:05:14 +0530

    Using makeStyles and useStyles Hook from '@mui/styles'

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,21 +1,48 @@
 import React from 'react';
+import {makeStyles} from '@mui/styles';
 import Typography from '@mui/material/Typography'
 import {Button,Container} from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 
+
+const useStyles = makeStyles({
+
+btn:{
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  fontSize: 100
+},
+title:{
+  textDecoration: 'underline',
+  marginBottom: 20
+}
+
+
+});
+
+
 export default function Create() {
+
+  const classes= useStyles();
+
   return (
    
 
       <Container>
 
      
-        <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        <Typography variant="h6" className={classes.title} component="h2" color="textSecondary" gutterBottom>
           Create a New Note
         </Typography>
 
         <Button
+        className={classes.btn}
         type="submit"
         color="secondary"
         variant="contained"
