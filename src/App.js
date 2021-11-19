f01b2d8a6f2915bb2e027c60a192ceea9954bd20
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-19 23:16:09 +0530

    Trying to use Custom Themes for Material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import Create from './pages/Create';
+import {createTheme, ThemeProvider ,useTheme} from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+const theme= createTheme({
+  palette:{
+    primary:{
+      main: green[500]
+    }
+  }
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+
+    
     <Router>
       <Switch>
         <Route exact path="/">
@@ -14,6 +26,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -8,20 +8,20 @@ import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const useStyles = makeStyles({
 
-btn:{
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
-  height: 48,
-  padding: '0 30px',
-  fontSize: 100
-},
-title:{
-  textDecoration: 'underline',
-  marginBottom: 20
-}
+// btn:{
+//   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+//   border: 0,
+//   borderRadius: 3,
+//   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+//   color: 'white',
+//   height: 48,
+//   padding: '0 30px',
+//   fontSize: 100
+// },
+// title:{
+//   textDecoration: 'underline',
+//   marginBottom: 20
+// }
 
 
 });
@@ -37,12 +37,12 @@ export default function Create() {
       <Container>
 
      
-        <Typography variant="h6" className={classes.title} component="h2" color="textSecondary" gutterBottom>
+        <Typography variant="h6"  component="h2" color="textSecondary" gutterBottom>
           Create a New Note
         </Typography>
 
         <Button
-        className={classes.btn}
+        
         type="submit"
         color="secondary"
         variant="contained"
