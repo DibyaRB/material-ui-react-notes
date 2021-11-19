20c814465cad0363681ba160981fd20203006695
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-19 23:54:12 +0530

    Adding text fields and validation.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Create from './pages/Create';
-import {createTheme, ThemeProvider ,useTheme} from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
-const theme= createTheme({
-  palette:{
-    primary:{
-      main: green[500]
-    }
-  }
-});
+
+
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+
 
     
     <Router>
@@ -26,7 +19,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
-    </ThemeProvider>
+
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,58 +1,113 @@
-import React from 'react';
-import {makeStyles} from '@mui/styles';
+import React, { useState } from 'react';
+import styled from 'styled-components'
+import { makeStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography'
-import {Button,Container} from '@mui/material';
+import { Button, Container } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { TextField } from '@mui/material';
+
+const CreateStyles = styled.div`
+    .field{
+      margin-top:  20px;
+      margin-bottom: 20px;
+      display: block;
+    }
+`;
+
+// const useStyles = makeStyles({
+
+// // btn:{
+// //   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+// //   border: 0,
+// //   borderRadius: 3,
+// //   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+// //   color: 'white',
+// //   height: 48,
+// //   padding: '0 30px',
+// //   fontSize: 100
+// // },
+// // title:{
+// //   textDecoration: 'underline',
+// //   marginBottom: 20
+// // }
+
+//   field : {
+//     marginTop: '20px',
+//     marginBottom: '20px',
+//     display: 'block'
+//   }
 
 
-
-const useStyles = makeStyles({
-
-// btn:{
-//   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-//   border: 0,
-//   borderRadius: 3,
-//   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//   color: 'white',
-//   height: 48,
-//   padding: '0 30px',
-//   fontSize: 100
-// },
-// title:{
-//   textDecoration: 'underline',
-//   marginBottom: 20
-// }
-
-
-});
+// });
 
 
 export default function Create() {
 
-  const classes= useStyles();
+
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError]= useState(false);
+  const [detailsError, setDetailsError]= useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if(title===''){
+      setTitleError(true);
+    }
+    if(details===''){
+      setDetailsError(true);
+    }
+    if (title && details) {
+      console.log(title, details);
+    }
+  }
+
+
 
   return (
-   
+
+    <CreateStyles>
+
 
       <Container>
 
-     
-        <Typography variant="h6"  component="h2" color="textSecondary" gutterBottom>
+
+        <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
           Create a New Note
         </Typography>
 
-        <Button
-        
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon/>}
- 
-        >
-          Submit
-        </Button>
-   
-        </Container>
-  
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField onChange={(e) => {
+            setTitle(e.target.value);
+          }} className="field" label="Note Title" variant="outlined" fullWidth required error={titleError}/>
+
+
+          <TextField className="field"
+            onChange={(e) => {
+              setDetails(e.target.value);
+            }}
+            label="Details" variant="outlined" fullWidth required multiline rows="4" error={detailsError}/>
+          <Button
+
+            type="submit"
+            color="secondary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+            error={detailsError}
+          >
+            Submit
+          </Button>
+        </form>
+
+
+
+      </Container>
+
+    </CreateStyles>
+
   )
 }
